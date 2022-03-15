31f90dcbfb3deb459e56e44ff94fecac73b02991
Moutasim EL Ayoubi <23410040+Moutasim02@users.noreply.github.com>
2022-03-15 23:27:01 +0400

    Hackerrank Strings Questions

---
+++ src/com/moutasim/PalindromeString.java
@@ -0,0 +1,26 @@
+package com.moutasim;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("String to check: ");
+        String A=sc.next();
+        String reversedString = "";
+        char character;
+
+        /* Enter your code here. Print output to STDOUT. */
+        for (int i = 0; i < A.length(); i++) {
+            character = A.charAt(i);
+            reversedString = character + reversedString;
+        }
+        System.out.println(reversedString);
+        if (A.equals(reversedString)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
